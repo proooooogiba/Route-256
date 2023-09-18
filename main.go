d23f2498f4dd44b94668a6a1251eb42ce143ca3b
Ivan Pogiba <pogibuskaivan@gmail.com>
2023-09-18 21:09:57 +0300

    [GOJHW-1-1]: homework-1-1 main and main_test files are updated

--- main.go
+++ main.go
@@ -1,1 +1,130 @@
-package homework_1_1
+package main
+
+import (
+	"errors"
+	"fmt"
+	"sort"
+)
+
+type Apple uint32
+
+type Bag struct {
+	Apple Apple
+}
+
+type Box struct {
+	Bags []Bag
+}
+
+type Car struct {
+	Boxes []Box
+}
+
+func applesToBag(appleNumber uint32) ([]Bag, error) {
+	if appleNumber == 0 {
+		return nil, errors.New("number of apples can't be zero")
+	}
+	var i uint32
+	var bags = make([]Bag, appleNumber)
+
+	for i = 0; i < appleNumber; i++ {
+		bags[i] = Bag{Apple: Apple(i + 1)}
+	}
+	return bags, nil
+}
+
+func packageBagsToBoxes(bags []Bag, numberOfApplesInBox uint32) ([]Box, error) {
+	if numberOfApplesInBox == 0 {
+		return nil, errors.New("number of apples in box can't be zero")
+	}
+
+	sort.Slice(bags, func(i, j int) bool {
+		return bags[i].Apple > bags[j].Apple
+	})
+
+	var numberOfBoxes, i uint32
+	bagsLength := uint32(len(bags))
+	numberOfBoxes = bagsLength / numberOfApplesInBox
+	var boxes = make([]Box, numberOfBoxes)
+
+	for i = 0; i < numberOfBoxes; i++ {
+		boxes[i] = Box{Bags: bags[i*numberOfApplesInBox : i*numberOfApplesInBox+numberOfApplesInBox]}
+	}
+
+	if bagsLength%numberOfApplesInBox == 0 {
+		return boxes, nil
+	}
+	numberOfApplesInLastBox := bagsLength % numberOfApplesInBox
+	boxes = append(boxes, Box{})
+	bagsLength++
+	boxes[numberOfBoxes] = Box{Bags: bags[bagsLength-1-numberOfApplesInLastBox : bagsLength-1]}
+
+	return boxes, nil
+}
+
+func boxesToCars(boxes []Box, numberOfCars uint32) ([]Car, error) {
+	if numberOfCars == 0 {
+		return nil, errors.New("number of cars can't be zero")
+	}
+
+	var i uint32
+	numberOfBoxes := uint32(len(boxes))
+	boxesInCar := numberOfBoxes / numberOfCars
+
+	var cars = make([]Car, numberOfCars)
+
+	for i = 0; i < numberOfCars; i++ {
+		cars[i] = Car{Boxes: make([]Box, boxesInCar)}
+	}
+
+	numberOfCarsWithExtraBox := numberOfBoxes % numberOfCars
+	for i = 0; i < numberOfCarsWithExtraBox; i++ {
+		cars[i] = Car{Boxes: make([]Box, boxesInCar+1)}
+	}
+
+	for i = 0; i < numberOfBoxes; i++ {
+		cars[i%numberOfCars].Boxes[i/numberOfCars] = boxes[i]
+	}
+
+	return cars, nil
+}
+
+func carsFormatString(cars []Car) string {
+	var formatString string
+	carNumber := len(cars)
+	for i := 0; i < carNumber; i++ {
+		boxNumber := len(cars[i].Boxes)
+		for j := 0; j < boxNumber; j++ {
+			formatString += fmt.Sprint("Машина: ", i+1, ", Ящик: ", j*carNumber+i+1, ", Яблоки:  [")
+			bagsNumber := len(cars[i].Boxes[j].Bags)
+			for k := 0; k < bagsNumber-1; k++ {
+				formatString += fmt.Sprint(cars[i].Boxes[j].Bags[k].Apple, ", ")
+			}
+			if bagsNumber > 0 {
+				formatString += fmt.Sprint(cars[i].Boxes[j].Bags[bagsNumber-1].Apple)
+			}
+			formatString += fmt.Sprint("]\n")
+		}
+	}
+	return formatString
+}
+
+func main() {
+	bags, err := applesToBag(100)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Error: %s", err))
+		return
+	}
+	boxes, err := packageBagsToBoxes(bags, 10)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Error: %s", err))
+		return
+	}
+	cars, err := boxesToCars(boxes, 2)
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Error: %s", err))
+		return
+	}
+	formatCarString := carsFormatString(cars)
+	fmt.Println(formatCarString)
+}
--- main_test.go
+++ main_test.go
@@ -1,1 +1,106 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_applesToBag(t *testing.T) {
+	type args struct {
+		appleNumber uint32
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []Bag
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := applesToBag(tt.args.appleNumber)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("applesToBag() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("applesToBag() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_boxesToCars(t *testing.T) {
+	type args struct {
+		boxes        []Box
+		numberOfCars uint32
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []Car
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := boxesToCars(tt.args.boxes, tt.args.numberOfCars)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("boxesToCars() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("boxesToCars() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_carsFormatString(t *testing.T) {
+	type args struct {
+		cars []Car
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := carsFormatString(tt.args.cars); got != tt.want {
+				t.Errorf("carsFormatString() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_packageBagsToBoxes(t *testing.T) {
+	type args struct {
+		bags                []Bag
+		numberOfApplesInBox uint32
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []Box
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := packageBagsToBoxes(tt.args.bags, tt.args.numberOfApplesInBox)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("packageBagsToBoxes() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("packageBagsToBoxes() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

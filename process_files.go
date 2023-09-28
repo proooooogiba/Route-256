8ff46435da608b363523141f8826eee576f3388b
Ivan Pogiba <pogibuskaivan@gmail.com>
2023-09-28 22:29:36 +0300

    [GOJHW-2]: homework-2 comments are corrected

--- process_files.go
+++ process_files.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"context"
 	"fmt"
 	"io"
 	"log"
@@ -13,20 +12,18 @@ import (
 )
 
 func main() {
-	ctx := context.Background()
-	if err := run(ctx); err != nil {
-		log.Println(ctx, err)
+	if err := run(); err != nil {
+		log.Println(err)
 		os.Exit(1)
 	}
-
-	log.Println(ctx, "exit")
+	log.Println("exit")
 }
 
 type line struct {
 	value string
 }
 
-func run(ctx context.Context) error {
+func run() error {
 	keyWords, err := createSliceOfKeyWordsFromFile("key_words.txt")
 	if err != nil {
 		return err
@@ -40,17 +37,23 @@ func run(ctx context.Context) error {
 
 	in := make(chan line)
 
-	go read(ctx, r, in)
+	go read(r, in)
 
-	out := make([]chan map[string]int, 2)
-	ctx = context.WithValue(ctx, "keyWords", keyWords)
-	for i := 0; i < len(out); i++ {
-		out[i] = make(chan map[string]int)
-		go process(ctx, in, out[i])
+	out := make(chan map[string]int)
+
+	var wg sync.WaitGroup
+	goroutinesNumber := 2
+	wg.Add(goroutinesNumber)
+	for i := 0; i < goroutinesNumber; i++ {
+		go process(in, out, keyWords, &wg)
 	}
 
-	mapOfQuantity := create(ctx, merge(out))
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
 
+	mapOfQuantity := create(out, keyWords)
 	w, err := os.OpenFile("output_test", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
 	if err != nil {
 		return err
@@ -66,30 +69,7 @@ func run(ctx context.Context) error {
 	return nil
 }
 
-func merge(in []chan map[string]int) chan map[string]int {
-	out := make(chan map[string]int)
-
-	var wg sync.WaitGroup
-
-	wg.Add(len(in))
-	for _, ch := range in {
-		go func(ch chan map[string]int) {
-			defer wg.Done()
-			for mapOfQuantity := range ch {
-				out <- mapOfQuantity
-			}
-		}(ch)
-	}
-
-	go func() {
-		wg.Wait()
-		close(out)
-	}()
-
-	return out
-}
-
-func read(ctx context.Context, r io.Reader, out chan<- line) {
+func read(r io.Reader, out chan<- line) {
 	s := bufio.NewScanner(r)
 	for s.Scan() {
 		out <- line{value: s.Text()}
@@ -97,29 +77,27 @@ func read(ctx context.Context, r io.Reader, out chan<- line) {
 	close(out)
 }
 
-func process(ctx context.Context, in <-chan line, out chan<- map[string]int) {
-
+func process(in <-chan line, out chan<- map[string]int, keyWords []string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for l := range in {
 		formatLine := strings.ToLower(l.value)
 		mapOfQuantity := make(map[string]int)
-		for _, keyWord := range ctx.Value("keyWords").([]string) {
+		for _, keyWord := range keyWords {
 			re := regexp.MustCompile(fmt.Sprintf("\\W*((?i)%s(?-i))\\W*", keyWord))
 			count := len(re.FindAllString(formatLine, -1))
 			mapOfQuantity[keyWord] = count
 		}
 		out <- mapOfQuantity
 	}
-	close(out)
 }
 
-func create(ctx context.Context, in <-chan map[string]int) map[string]int {
+func create(in <-chan map[string]int, keyWords []string) map[string]int {
 	mapOfResults := make(map[string]int)
 	for mapOfQuantity := range in {
-		for _, keyWord := range ctx.Value("keyWords").([]string) {
+		for _, keyWord := range keyWords {
 			mapOfResults[keyWord] += mapOfQuantity[keyWord]
 		}
 	}
-
 	totalScore := 0
 	for _, val := range mapOfResults {
 		totalScore += val
@@ -139,12 +117,8 @@ func createSliceOfKeyWordsFromFile(fileNameKeyWords string) ([]string, error) {
 
 	scanner := bufio.NewScanner(fileWithKeyWords)
 	for scanner.Scan() {
-		wordWithDelimiter := strings.Split(scanner.Text(), " ")
-		for _, word := range wordWithDelimiter {
-			if word != "-" {
-				keyWords = append(keyWords, strings.ToLower(word))
-			}
-		}
+		word := strings.ToLower(scanner.Text())
+		keyWords = append(keyWords, word)
 	}
 	return keyWords, nil
 }
